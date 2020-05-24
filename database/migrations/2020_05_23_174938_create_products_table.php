57c54622b2a9a40d5117f0d371ebf96bedf28f9c
Antonio Gil <antonioluisgil@gmail.com>
2020-05-24 19:31:05 +0200

    Rename product name column to title

--- database/migrations/2020_05_23_174938_create_products_table.php
+++ database/migrations/2020_05_23_174938_create_products_table.php
@@ -15,7 +15,7 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('title');
             $table->string('description', 1000);
             $table->float('price')->unsigned();
             $table->integer('stock')->unsigned();
