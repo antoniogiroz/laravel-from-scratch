c52ea672b0baf6b3f898918568360511603f529e
Antonio Gil <antonioluisgil@gmail.com>
2020-05-25 18:55:51 +0200

    Add links to navigate between product views

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -3,6 +3,8 @@
 @section('content')
 <h1>List of products</h1>
 
+<a class="btn btn-primary" href="{{ route('products.create') }}">New product</a>
+
 @empty($products)
 <div class="alert alert-warning">
     The product list is empty
@@ -17,6 +19,7 @@
             <th>Price</th>
             <th>Stock</th>
             <th>Status</th>
+            <th>Actions</th>
         </thead>
 
         <tbody>
@@ -28,6 +31,10 @@
                 <td>{{ $product->price }}</td>
                 <td>{{ $product->stock }}</td>
                 <td>{{ $product->status }}</td>
+                <td>
+                    <a class="btn btn-link" href="{{ route('products.show', $product->id) }}">View</a>
+                    <a class="btn btn-link" href="{{ route('products.edit', $product->id) }}">Edit</a>
+                </td>
             </tr>
             @endforeach
         </tbody>
