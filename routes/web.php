67aa8c10a0f9f9d283163fba537edd031d9f7392
Antonio Gil <antonioluisgil@gmail.com>
2020-05-30 13:49:47 +0200

    Install laravel ui and generate resources

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,7 @@ Route::get('products/{product}', 'ProductController@show')->name('products.show'
 Route::get('products/{product}/edit', 'ProductController@edit')->name('products.edit');
 Route::match(['put', 'patch'], 'products/{product}', 'ProductController@update')->name('products.update');
 Route::delete('products/{product}', 'ProductController@destroy')->name('products.destroy');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
