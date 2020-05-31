5d9c445815df61143f316f69cafb886ca921f79f
Antonio Gil <antonioluisgil@gmail.com>
2020-05-31 07:40:44 +0200

    Create product-card component and show in welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,4 +2,16 @@
 
 @section('content')
 <h1>Welcome page</h1>
+
+@empty($products)
+No products.
+@else
+<div class="row">
+    @foreach ($products as $product)
+    <div class="col-3">
+        @include('components.product-card')
+    </div>
+    @endforeach
+</div>
+@endempty
 @endsection
